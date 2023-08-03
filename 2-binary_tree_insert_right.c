28a7e816151880c4d595f45fc28137d1e6657256
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-08-03 15:30:28 -0700

    Pre-order traversal

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: Value to store in the new node
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	right_node = malloc(sizeof(binary_tree_t));
+
+	if (right_node == NULL)
+		return (NULL);
+
+	right_node->n = value;
+	right_node->parent = parent;
+	right_node->left = NULL;
+
+	if (parent->right != NULL)
+	{
+		right_node->right = parent->right;
+		parent->right->parent = right_node;
+	}
+	else
+	{
+		right_node->right = NULL;
+	}
+	parent->right = right_node;
+
+	return (right_node);
+}
