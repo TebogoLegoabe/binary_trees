d6fd20bf83ce46b9dffb92213b0c366e5fd3c9e6
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-08-03 17:36:30 -0700

    edited

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,9 +10,15 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	int temp;
+	size_t depth = 0;
 
-	for (temp = 0; node && node->parent; ++temp)
-		node = node->parent;
-	return (temp);
+	if (tree == NULL)
+		return (0);
+
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
 }
